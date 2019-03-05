441bee2f61396f1471f5c6b2597de389631a001b
camcody <47641378+camcody@users.noreply.github.com>
2019-03-05 15:52:39 -0500

    Update homeScreen.java

--- ASL Java Code/homeScreen.java
+++ ASL Java Code/homeScreen.java
@@ -1,28 +1,55 @@
-import java.*;
+//import java.*;
 import javax.swing.*; //JButton, Jlabel, Jpanel
-//import java.awt.*;
+import java.awt.*;
 import java.awt.event.*; //actionlistener
 /**
  * Started by Cameron
+ * Edited by Cameron on March 5th
  */
-public class homeScreen{
-    private JPanel keyPanel, clearpanel;
-    private JLabel loginLabel, registerLabel;
-    private String appTitle;
-    public homeScreen(String s){
-        this.appTitle = s;
+public class homeScreen extends JPanel{
+    private JPanel keyPanel;
+    private JLabel greeting; //text in the frame
+    private JButton login, register;
+    private JFrame frame = new JFrame("ASL - American Sign Language");
+
+    public homeScreen(){
+        setLayout(new BorderLayout());
+        keyPanel = new JPanel(new GridLayout(4, 3));
+        greeting = new JLabel("Welcome to the ASL App!");//message at top of screen
+
+        //creates buttons 
+        login = new JButton("Click to Log In");
+        register = new JButton("Click to Register");
+        
+        //adds buttons to the panel display
+        keyPanel.add(login);
+        keyPanel.add(register);
+        
+        // When button clicked, moves to login or register screen
+        login.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent event){
+                frame.dispose();
+                new loginScreen();
+            }
+        });
+        
+        register.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent event){
+                frame.dispose();
+                new registerScreen();
+            }
+        });
+        
+        //components in the frame
+        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        frame.add(greeting, BorderLayout.NORTH);
+        frame.add(new JLabel(new ImageIcon("C:/Users/cam36/Desktop/CSCI 401/ASL/ASL_Logo.png")), BorderLayout.CENTER);
+        frame.add(keyPanel, BorderLayout.SOUTH);
+        frame.setSize(300, 400); //size of the frame/phone
+        frame.setVisible(true);
     }
-    public void loginButton(){
+
+    public static void main(String[] args){
+        new homeScreen();
     }
-    public void registerButton(){
-    }
-    private class KeyListener implements ActionListener{
-        //-----------------------------------------------------------------
-        // Appends the key value to the display.
-        //-----------------------------------------------------------------
-        public void actionPerformed(ActionEvent event){
-            JButton source = (JButton)event.getSource();
-            loginLabel.setText(loginLabel.getText() + source.getText());
-        }
-    } 
 }
