78538c9188f07885cb1bfc9a889269bb698d0d22
rpmiller12 <47118572+rpmiller12@users.noreply.github.com>
2019-03-05 21:59:40 -0500

    Part 2 of tonight's (3/5) commit

--- app/src/main/java/com/example/signit/MainActivity.java
+++ app/src/main/java/com/example/signit/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.signit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login = (Button) findViewById(R.id.login);
+        Button createAccount = (Button) findViewById(R.id.createAccount);
+
+        login.setOnClickListener(this);
+        createAccount.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        Intent nextActivity;
+        switch (v.getId()) {
+            case R.id.login: //Start the login activity
+                nextActivity = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(nextActivity);
+                break;
+            case R.id.createAccount: //Start the account creation
+                nextActivity = new Intent(MainActivity.this, CreateAccountActivity.class);
+                MainActivity.this.startActivity(nextActivity);
+                break;
+        }
     }
 }
