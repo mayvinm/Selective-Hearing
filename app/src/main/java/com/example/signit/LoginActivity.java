490b691723a3e016bcc9532b43f20ce215e42d09
rpmiller12 <47118572+rpmiller12@users.noreply.github.com>
2019-03-06 02:17:12 -0500

    Layout cleaned up

--- app/src/main/java/com/example/signit/LoginActivity.java
+++ app/src/main/java/com/example/signit/LoginActivity.java
@@ -2,12 +2,27 @@ package com.example.signit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button submitLogin = (Button) findViewById(R.id.submitLogin);
+        submitLogin.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        EditText usernameField = (EditText) findViewById(R.id.usernameField);
+        EditText passwordField = (EditText) findViewById(R.id.passwordField);
+
+        String enteredUname = ((SpannableStringBuilder)usernameField.getText()).toString();
+        String enteredPword = ((SpannableStringBuilder)passwordField.getText()).toString();
     }
 }
