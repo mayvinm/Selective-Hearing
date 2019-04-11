677d06dddb71dd9d24d45638ebc89ed3556aaa37
rpmiller12 <47118572+rpmiller12@users.noreply.github.com>
2019-04-11 13:17:11 -0400

    Add files via upload

---
+++ ASL Java Code/User.java
@@ -0,0 +1,26 @@
+
+public class User {
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String email;
+	
+	public User(String firstName, String lastName, String username, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.email = email;
+	}
+	public String getFirstName() {
+		return this.firstName;
+	}
+	public String getLastName() {
+		return this.lastName;
+	}
+	public String getUsername() {
+		return this.username;
+	}
+	public String getEmail() {
+		return this.email;
+	}
+}
