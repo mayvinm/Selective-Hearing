3acd40f1d628a2b705471aecf8e0e4c86fd00cc3
camcody <47641378+camcody@users.noreply.github.com>
2019-04-15 11:47:18 -0400

    Update Lesson.java

--- ASL Java Code/Lesson.java
+++ ASL Java Code/Lesson.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 /**
  * Started by Cameron on March 5th
+ * Edited by Cameron on April 3rd
  */
 public class Lesson{
     private JFrame frame = new JFrame("App Lessons");
@@ -10,15 +11,16 @@ public class Lesson{
     private JLabel greetText = new JLabel("Greetings Lesson");
     private JLabel numberText = new JLabel("Numbers Lesson");
     private JLabel alphaText = new JLabel("Alphabet Lesson");
+    private JButton quizButton = new JButton("Go to Quizes");
     private JButton greetButton = new JButton("Begin");
     private JButton numberButton = new JButton("Begin");
     private JButton alphaButton = new JButton("Begin");
     //private JButton anotherlessonButton = new JButton("Begin");
     public Lesson(){
-        panel.setLayout(new FlowLayout(1));
-        greetButton.setPreferredSize(new Dimension(80, 50));
-        numberButton.setPreferredSize(new Dimension(80, 50));
-        alphaButton.setPreferredSize(new Dimension(80, 50));
+        panel.setLayout(new GridLayout(4, 2));
+        //greetButton.setPreferredSize(new Dimension(80, 50));
+        //numberButton.setPreferredSize(new Dimension(80, 50));
+        //alphaButton.setPreferredSize(new Dimension(80, 50));
         
         greetButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent event){
@@ -37,19 +39,30 @@ public class Lesson{
         alphaButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent event){
                 frame.dispose();
-                //put alphabet contrustor here;
+                //put alphabet constructor here;
             }
         });
+        
+        quizButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent event){
+                frame.dispose();
+                new Quiz();
+            }
+        });
+
         panel.add(alphaText);
         panel.add(alphaButton);
         panel.add(numberText);
         panel.add(numberButton);
         panel.add(greetText);
         panel.add(greetButton);
+        panel.add(quizButton);
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setContentPane(panel);
-        frame.setSize(300, 400);
+        frame.pack();
         frame.setVisible(true);
     }
 }
+
