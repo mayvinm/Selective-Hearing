748ccd564e1c0f7d87ee91b8a39ef8165cdb8cce
camcody <47641378+camcody@users.noreply.github.com>
2019-04-24 18:36:21 -0400

    Update Lesson.java

--- ASL Java Code/Lesson.java
+++ ASL Java Code/Lesson.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 /**
  * Started by Cameron on March 5th
- * Edited by Cameron on April 3rd
+ * Edited by Cameron on April 3rd and April 24th
  */
 public class Lesson{
     private JFrame frame = new JFrame("App Lessons");
@@ -11,16 +11,18 @@ public class Lesson{
     private JLabel greetText = new JLabel("Greetings Lesson");
     private JLabel numberText = new JLabel("Numbers Lesson");
     private JLabel alphaText = new JLabel("Alphabet Lesson");
+    private JLabel foodText = new JLabel("Food Lesson");
     private JButton quizButton = new JButton("Go to Quizes");
     private JButton greetButton = new JButton("Begin");
     private JButton numberButton = new JButton("Begin");
     private JButton alphaButton = new JButton("Begin");
-    //private JButton anotherlessonButton = new JButton("Begin");
+    private JButton foodButton = new JButton("Begin");
     public Lesson(){
         panel.setLayout(new GridLayout(4, 2));
-        //greetButton.setPreferredSize(new Dimension(80, 50));
-        //numberButton.setPreferredSize(new Dimension(80, 50));
-        //alphaButton.setPreferredSize(new Dimension(80, 50));
+        greetButton.setPreferredSize(new Dimension(80, 50));
+        numberButton.setPreferredSize(new Dimension(80, 50));
+        alphaButton.setPreferredSize(new Dimension(80, 50));
+        foodButton.setPreferredSize(new Dimension(80, 50));
         
         greetButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent event){
@@ -39,7 +41,14 @@ public class Lesson{
         alphaButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent event){
                 frame.dispose();
-                //put alphabet constructor here;
+                //put alphabet lesson constructor here;
+            }
+        });
+        
+        foodButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent event){
+                frame.dispose();
+                new FoodLesson();
             }
         });
         
@@ -56,6 +65,8 @@ public class Lesson{
         panel.add(numberButton);
         panel.add(greetText);
         panel.add(greetButton);
+        panel.add(foodText);
+        panel.add(foodButton);
         panel.add(quizButton);
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -65,4 +76,3 @@ public class Lesson{
         frame.setVisible(true);
     }
 }
-
